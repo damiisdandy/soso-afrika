2777a232f083a8c17bf2bfce6fb701077568d455
Jplayskeys <joshuaadeniyi2000@gmail.com>
2023-03-28 15:48:18 +0100

    fixes: UI

--- components/hero-cards/index.tsx
+++ components/hero-cards/index.tsx
@@ -7,12 +7,12 @@ type HerocardProps = {
 };
 const HeroCards: React.FC<HerocardProps> = ({ img, title }) => {
   return (
-    <div className="relative">
+    <div className="relative h-fit">
       <Image
         src={img}
         alt={`${title} image`}
         // className="w-full h-auto"
-        className="min-w-[322px] h-[303px] xl:h-[254px]"
+        className="min-w-[250px] min-h-[200px]  sm:min-w-[322px] sm:h-[303px] xl:h-[254px] h-[200px]"
       />
       <h2 className="p-3 absolute bottom-20 left-6 font-bold">{title}</h2>
     </div>
