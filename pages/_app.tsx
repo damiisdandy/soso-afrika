232b45c287f49dbabefee8a15449b2bed99d2072
damilola jerugba <damilola.jerugba@gmail.com>
2023-04-04 14:26:33 +0100

    Revert "Feat/UI sweep"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
-import Header from "@/components/navbar";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class">
-      <Header />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />
 }
