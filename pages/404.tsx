6e6efc71e152276327bebedbaad39b782070e0f4
Jplayskeys <joshuaadeniyi2000@gmail.com>
2023-04-12 19:39:05 +0100

    built the custom 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,17 @@
 import { Custom404Img } from "@/assets";
 import React from "react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function Custom404() {
   return (
-    <main className="grid place-items-center h-[100vh]">
-      <Custom404Img />
-      <h2 className="text-3xl font-bold">Page Not Found</h2>
+    <main className="grid place-items-center h-[100vh] overflow-hidden">
+      <Player
+        src="https://assets10.lottiefiles.com/private_files/lf30_conoeouc.json"
+        loop
+        autoplay
+        className="max-w-[40rem] max-h-[35rem]"
+      />
+      <h2 className="text-2xl md:text-3xl font-bold">Page Not Found</h2>
     </main>
   );
 }
