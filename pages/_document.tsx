c1e881f3b23a663fa1b956bfb583776ccc101610
Damilola Jerugba <hello@damiisdandy.com>
2023-07-23 17:19:00 +0100

    Added google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -21,6 +22,25 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script
+          strategy="beforeInteractive"
+          src="https://apis.google.com/js/api.js"
+        />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+              });
+          `}
+        </Script>
       </body>
     </Html>
   );
